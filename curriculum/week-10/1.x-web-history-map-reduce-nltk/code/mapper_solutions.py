53e5465cdbaae5016a0ffa0552dad233eeb0c7cb
powchow <pauline.chow@gmail.com>
2016-08-07 20:35:48 +0000

    updated code

--- curriculum/week-10/1.x-web-history-map-reduce-nltk/code/mapper_solutions.py
+++ curriculum/week-10/1.x-web-history-map-reduce-nltk/code/mapper_solutions.py
@@ -2,23 +2,25 @@
 
 import sys
 import string
+import urlparse
+
+
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
-    words = line.split()
+    urls = line.split()
     # increase counters
-    for word in words:
+    for url in urls:
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-        punct = lambda x:x not in string.punctuation
-        nonum = lambda x:x not in string.digits
 
-        word = filter(punct, word.lower())
-        word = filter(nonum, word.lower())
-        print '%s\t%s' % (filter(punct, word.lower()), 1)
+	parsed = urlparse.urlparse(url)
+        #word = filter(punct, word[0].lower()) #takes the first entry in the list of urls found
+        #word = filter(nonum, word.lower())
+        print '%s\t%s' % (parsed[1], 1)
